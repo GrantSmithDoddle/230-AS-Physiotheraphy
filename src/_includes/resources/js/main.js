efe16388884b136aab0c0ac2ddf24f85f0ec0f22
Grant Smith <grant@transition-creative.co.uk>
2020-10-29 16:59:11 +0000

    Fixed page on hamburger click

--- src/_includes/resources/js/main.js
+++ src/_includes/resources/js/main.js
@@ -1,5 +1,16 @@
 // Footer Date
 document.getElementById('year').innerHTML = new Date().getFullYear();
 
+// Disable Scroll on Hamburger Click
+document.addEventListener("DOMContentLoaded", () => {
+	let hamBurger = document.querySelector(".hamburger");
+
+	// On click
+	hamBurger.addEventListener("click", function() {
+		// disable body scroll
+		document.body.classList.toggle('noScroll');
+	});
+});
+
 import 'lazysizes';
 import './partials/_browser-update';

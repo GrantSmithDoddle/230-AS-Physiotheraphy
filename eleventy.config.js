0e0d0db36441fcf02b775cdf078e6308e24eb19f
Grant Smith <grant@transition-creative.co.uk>
2020-11-03 08:55:42 +0000

    Updated URLS

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,7 +11,7 @@ module.exports = eleventyConfig => {
     // Generate Sitemap
     eleventyConfig.addPlugin(sitemap, {
       sitemap: {
-        hostname: "https://asphysiotherapy.co.uk",
+        hostname: "https://www.asphysiotherapy.co.uk",
       },
     });
 
